50734de5f3313f4ec9f4eee28d6724335bccd875
Ritish Mishra <ritish0044@gmail.com>
2026-08-01 21:11:42 +0530

    pattern printing

---
+++ Basics/patternS.py
@@ -0,0 +1,32 @@
+# *
+# **
+# ***
+# ****
+# *****
+for i in range(8):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+for i in range(8):
+    for j in range(i):
+        print(j+1,end= " ")
+    print()
+
+# 1
+# 22
+# 333
+# 444
+# 555
+for i in range(7):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+
